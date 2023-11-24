ab42a5df396fe3925198845496086f4e87a84b02
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2023-11-24 14:07:39 +0330

    add __main__ to use the package as `python -m timetp '...' [-v]`

---
+++ __main__.py
@@ -0,0 +1,16 @@
+import sys
+
+from .timetp import parse, tokenize
+
+if __name__ == '__main__':
+    time = sys.argv[1]
+    verbose = False
+    try:
+        verbose = True if sys.argv[2] == '-v' else False
+    except IndexError:
+        pass
+
+    if verbose:
+        print(repr(parse(tokenize(time))))
+    else:
+        print(parse(tokenize(time)))
