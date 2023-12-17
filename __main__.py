8020839e29641bf362de79ddf16a6b5037fff772
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2023-12-17 15:39:28 +0330

    add help to __main__

--- __main__.py
+++ __main__.py
@@ -1,9 +1,29 @@
 import sys
 
-from .timetp import parse, tokenize
+from .timetp import parse, tokenize, letters
+
+keys = ', '.join(letters.keys())
+values = ', '.join(letters.values())
 
 if __name__ == "__main__":
-    time = sys.argv[1]
+    usage = f"""timetp. Little tokenizer and parser with finite automata studies
+USAGE: python -m timetp "TIME-STRING" [-v]
+       python -m timetp help
+
+help:
+    TIME-STRING:    a sequence of digitletters like: 4w5d16h99m10s
+                    - first digit must not be 0
+                    - letters can be chosen from: {keys!r} 
+                      which correspond to {values!r}
+    help:           show this message and exit
+    -v:             enable verbose mode"""
+
+    time_or_help = sys.argv[1]
+    if time_or_help == 'help':
+        print(usage)
+        sys.exit(0)
+
+    time = time_or_help
     verbose = False
     try:
         verbose = True if sys.argv[2] == "-v" else False
